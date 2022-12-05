bb77cc2342905c6c943689ec88c891e0c044c272
adamclear <3589@holbertonschool.com>
2022-12-05 20:30:37 +0000

    updated

--- populate.py
+++ populate.py
@@ -1,16 +1,53 @@
 #!/usr/bin/python3
 """ Script to populate MYSql DB using Pandas """
 import pandas
+import csv
+import string
 
 readColors = [
-    3, 4, 5
+    2, 3, 4, 5, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27
 ]
-
 nameColors = [
-    'Title',
-    'Season',
-    'Episode'
+    'Thumbnail', 'Title', 'Season', 'Episode', 'Youtube',
+    'Black Gesso', 'Bright Red', 'Burnt Umber', 'Cadmium Yellow',
+    'Dark Sienna', 'Indian Red', 'Indian Yellow', 'Liquid Black',
+    'Liquid Clear', 'Midnight Black', 'Phthalo Blue', 'Phthalo Green',
+    'Prussian Blue', 'Sap Green', 'Titanium White', 'Van Dyke Brown',
+    'Yellow Ochre', 'Alizarin Crimson'
 ]
-dataFrame = pandas.read_csv("data_files/Colors", usecols=readColors, names=nameColors, skiprows=[0])
+colorFrame = pandas.read_csv("data_files/Colors",
+                              usecols=readColors,
+                              names=nameColors,
+                              skiprows=[0])
 
-print(dataFrame)
+readSubjects = [x for x in range(2, 69)]
+with open('data_files/Subjects') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    nameSubjects = []
+    for row in csv_reader:
+        nameSubjects.append(row)
+        break
+del nameSubjects[0][0:2]
+for x in range(len(nameSubjects[0])):
+    new_header = string.capwords(nameSubjects[0][x].replace('_', ' ').lower())
+    nameSubjects[0][x] = new_header
+subjectFrame = pandas.read_csv("data_files/Subjects",
+                                usecols=readSubjects,
+                                names=nameSubjects[0],
+                                skiprows=[0])
+
+finalDateList = []
+with open('data_files/Dates') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ' ')
+    dateList = []
+    for row in csv_reader:
+        dateList.append(row)
+for x in range(len(dateList)):
+    dateMonth = dateList[x][1][1:]
+    dateDay = dateList[x][2].replace(',', '')
+    dateYear = dateList[x][3].replace(')', '')
+    finalDateList.append([dateMonth, dateDay, dateYear])
+dateFrame = pandas.DataFrame(finalDateList, columns=['Month', 'Day', 'Year'])
+
+mergedFrame = pandas.concat([colorFrame, subjectFrame, dateFrame], axis=1)
